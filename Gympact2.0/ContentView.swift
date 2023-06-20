5e8e328ced9ab800ef0c91cf069b0296902069f3
Jordan Bhar <48815489+JordanBhar@users.noreply.github.com>
2023-06-20 15:24:39 -0400

    Initial Commit

---
+++ Gympact2.0/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Gympact2.0
+//
+//  Created by Jordan Bhar on 2023-06-20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Gympact2.0/Gympact2_0App.swift
@@ -0,0 +1,17 @@
+//
+//  Gympact2_0App.swift
+//  Gympact2.0
+//
+//  Created by Jordan Bhar on 2023-06-20.
+//
+
+import SwiftUI
+
+@main
+struct Gympact2_0App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
